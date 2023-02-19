f321ef010dbde1c8ee79116b7751df70f2cd4a2f
Labid <59018124+labidz@users.noreply.github.com>
2023-02-19 12:57:41 +0600

    Create linear.cpp

---
+++ linear.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <chrono>
+#include <iomanip>
+
+struct Node{
+    long long data;
+    Node *next;
+};
+Node *head = NULL;
+
+void insertEnd(long long x){
+    Node *ptr = new Node();
+    ptr->data=x;
+    if(head==NULL)
+        head=ptr;
+    else{
+        Node *temp = head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=ptr;
+        ptr->next=NULL;
+    }
+}
+int search(int key){
+    auto start_time = std::chrono::high_resolution_clock::now();
+    Node *temp = head;
+    while(temp != NULL){
+        if(temp->data == key){
+            auto end_time = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+            std::cout << "Execution time: " << std::fixed << std::setprecision(2) << duration.count() << " ms\n";
+            return temp->data;
+        }
+        else temp = temp->next;
+    }
+
+    return -1;
+}
+
+int main(int argc, char **argv) {
+    long long n;std::cin>>n;
+    for(long long i=0;i<n;i++){
+        //int a; std::cin>>a;
+        insertEnd(i);
+    }
+
+    std::cout<<"Enter Key: ";
+    int key; std::cin>>key;
+
+
+    int x = search(key);
+
+    if(x==-1) std::cout<<"NotFound"<<std::endl;
+    else std::cout<<"Found"<<std::endl;
+    return 0;
+}
