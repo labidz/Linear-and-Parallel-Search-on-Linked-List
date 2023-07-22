ce039d6de9d1a8c4829c6bda2846d5871891769d
Labid <59018124+labidz@users.noreply.github.com>
2023-07-22 21:14:21 +0600

    Update parallelV2.cpp

--- parallelV2.cpp
+++ parallelV2.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "common.h"
 #include "unordered_map"
+#include <future>
 
 
 int ck = 0, n=0;
@@ -32,6 +33,7 @@ std::unordered_map<int, std::vector<Node *>> chunk_cache;
 std::unordered_map<int, Node *> search_cache;
 
 
+
 void parallelSearch(int key, unsigned int num_threads) {
     auto start_time = start_tick();
     stop_thread = false;
@@ -39,47 +41,53 @@ void parallelSearch(int key, unsigned int num_threads) {
     auto c_result = search_cache.find(key);
     if (c_result != search_cache.end()) {
         ck = 1;
-        std::cout << "Found a match in cache:: k: " << c_result->first <<std::endl;// " v: " << c_result->second->data << std::endl;
+        std::cout << "Found a match in cache:: k: " << c_result->first << std::endl;
         end_tick(start_time);
         return;
     }
 
     auto chunk_size = (n + num_threads - 1) / num_threads;
-
-    std::vector<std::thread> threads(num_threads);
-    Node result;
+    std::vector<std::future<void>> futures;
+    std::vector<Node> results(num_threads);
 
     // Launch the threads to search for the key in each chunk
-    Node *start = head;
+    Node* start = head;
     for (int i = 0; i < num_threads; i++) {
         auto c_ptr = chunk_cache.find(i);
         if (c_ptr != chunk_cache.end()) {
-            threads[i] = std::thread(search, c_ptr->second[0], c_ptr->second[1], key, std::ref(result));
-            threads[i].join();
+            futures.emplace_back(std::async(std::launch::async, search, c_ptr->second[0], c_ptr->second[1], key, std::ref(results[i])));
         } else {
-            Node *Nend = start;
+            Node* Nend = start;
             for (int j = 0; j < chunk_size && Nend != nullptr; j++) {
                 Nend = Nend->next;
             }
-            threads[i] = std::thread(search, start, Nend, key, std::ref(result));
-            //Cache chunk boundaries
-            std::vector<Node *> tmp;
+            futures.emplace_back(std::async(std::launch::async, search, start, Nend, key, std::ref(results[i])));
+            // Cache chunk boundaries
+            std::vector<Node*> tmp;
             tmp.push_back(start);
             tmp.push_back(Nend);
             chunk_cache.insert(std::make_pair(i, tmp));
             start = Nend;
-            // join thread to main thread
-            threads[i].join();
         }
     }
 
+    // Wait for all threads to finish
+    for (auto& future : futures) {
+        future.wait();
+    }
+
+    // Check the results
+    for (int i = 0; i < num_threads; i++) {
+        if (results[i].data == key) {
+            search_cache.insert(std::make_pair(key, &results[i]));
+            ck = 1;
+            std::cout << "Found a match:: k: " << key << " v: " << results[i].data << std::endl;
+            break;
+        }
+    }
 
     if (ck == 0) {
         std::cout << "Not Found" << std::endl;
-    } else {
-        search_cache.insert(std::make_pair(key, &result));
-        std::cout << "Found a match:: k: " << key << " v: " << result.data << std::endl;
-
     }
 
     end_tick(start_time);
